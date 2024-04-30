4a5b80017e4e2e2958a9f802f2a50d14b25c2713
Tara Bahadur Thapa <tarabahadur.thapa@gmail.com>
2024-04-30 06:12:58 +0545

    Initial Set up
    Initial Set up  of Chautari Chatter

--- chautarichatter_backend/src/main/java/com/chautarichatter/chautarichatter/ChautarichatterApplication.java
+++ chautarichatter_backend/src/main/java/com/chautarichatter/chautarichatter/ChautarichatterApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChautarichatterApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ChautarichatterApplication.class, args);
 	}
