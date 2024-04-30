ac4f611d8f9bcdb98995c6b1ab68b6907ecf1b34
LAPTOP-1RT688NU\tarab <tara.thapa@securin.io>
2024-04-30 06:11:42 +0545

    Initial Set Up

---
+++ chautarichatter_backend/src/main/java/com/chautarichatter/chautarichatter/ChautarichatterApplication.java
@@ -0,0 +1,13 @@
+package com.chautarichatter.chautarichatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChautarichatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChautarichatterApplication.class, args);
+	}
+
+}
---
+++ chautarichatter_backend/src/test/java/com/chautarichatter/chautarichatter/ChautarichatterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chautarichatter.chautarichatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChautarichatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
